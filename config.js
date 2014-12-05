a2bf5de2f9c185e75c8c7bfbc9909b8bd53dd8e6
alberto <alberto.rodriguez@biko2.com>
2014-12-05 16:49:11 +0100

    Make config_overrides.json optional

--- config.js
+++ config.js
@@ -1,6 +1,13 @@
 var _ = require('underscore');
 var config = require('./config.json');
-var local_config = require('./config_overrides.json');
+var fs = require('fs');
+
+var local_config = {}
+var localFilePath = './config_overrides.json';
+if (fs.existsSync(localFilePath)) {
+    local_config = require(localFilePath);
+}
+
 _.extend(config, local_config);
 
 module.exports = config;
